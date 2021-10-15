835b0cfca8468e886d4d8822c5c55b9a9299a88c
Sergio Rios <SDRiosPerez@outlook.com>
2021-10-15 10:41:51 -0500

    Created the initial traffic light modbust test client and server

--- Pi/client/modbus_test_client.py
+++ Pi/client/modbus_test_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-
+from time import sleep
+from sense_hat import SenseHat
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
 # Error Logging
@@ -13,17 +14,41 @@ log.setLevel(logging.DEBUG)
 # Define slave ID
 UNIT = 0x1
 
+traffic_light = {
+    "green": (0,255,0),
+    "yellow": (255,255,0),
+    "red": (255,0,0)
+}
+
 def run_client():
     client = ModbusClient('localhost', port=5020)
     client.connect()
 
-    log.debug("Reading Coil 1")
-    coil = client.read_coils(1, 8, unit=UNIT)
-    print(dir(coil))
-    print(coil.function_code)
-    print(coil.protocol_id)
-    print(coil.bits)
+    curr_color = "green"
+    sense = SenseHat()
+    sense.clear(traffic_light[curr_color])
+
+    while(True):
+        log.debug("Reading Coil 1")
+        coil = client.read_coils(1, 8, unit=UNIT)
+        print(dir(coil))
+        print(coil.function_code)
+        print(coil.protocol_id)
+        print(coil.bits)
+
+        if(coil.bits[0] == True):    
+            if(curr_color == "green"):
+                curr_color = "yellow"
+            elif(curr_color == "yellow"):
+                curr_color = "red"
+            elif(curr_color == "red"):
+                curr_color = "green"
+
+        sense.clear(traffic_light[curr_color])
+        sleep(2)
+
     client.close()
+
 
 if __name__ == "__main__":
     run_client()
